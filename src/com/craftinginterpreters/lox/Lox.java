1b2f5405353d355c31af5796c85b9e859b9dea6b
Miguel <nkonondememiguel7@gmail.com>
2024-06-08 01:29:09 +0200

    Finish the Scanner

--- src/com/craftinginterpreters/lox/Lox.java
+++ src/com/craftinginterpreters/lox/Lox.java
@@ -8,7 +8,6 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
-import java.util.Scanner;
 
 public class Lox {
     static boolean hadError = false;
